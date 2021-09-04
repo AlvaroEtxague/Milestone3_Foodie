68a1c71d4067c9bddcd58b5171aefcde09acbdf1
Tinowski <alvaroetxague@gmail.com>
2021-09-04 18:14:20 +0100

    PEP8 fixes mongo.py

--- website/mongo.py
+++ website/mongo.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 from flask_pymongo.wrappers import MongoClient
 if os.path.exists("env.py"):
     import env
-    
+
 mongo = Blueprint('mongo', __name__)
 
 # mapping the mongo cluster from env.py
@@ -16,6 +16,7 @@ DB = os.environ.get("MONGO_DBNAME")
 # mapping the collections from env.py
 COLLECTION_USERS = os.environ.get("COLLECTION_USERS")
 COLLECTION_POSTS = os.environ.get("COLLECTION_POSTS")
+
 
 # function to check db connection
 def mongo_connect(url):
